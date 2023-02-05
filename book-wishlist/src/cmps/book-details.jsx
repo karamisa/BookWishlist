ed78c002812a10afa3f2584724c2e380c6f7dbef
Elichay Kaplan <Elichay_Kaplan@Yahoo.com>
2023-02-05 14:25:50 +0200

    book details basic

--- book-wishlist/src/cmps/book-details.jsx
+++ book-wishlist/src/cmps/book-details.jsx
@@ -1,7 +1,49 @@
-export function BookDetails(){
+import { useState } from "react"
+
+
+
+export function BookDetails({ books, onToggleWishlist }) {
+    const [index, setIndex] = useState(0)
+
+
+    function onRightArrow() {
+        setIndex(index + 1)
+    }
+
+    function onLeftArrow() {
+        setIndex(index - 1)
+    }
+
+
+
     return (
-        <div>
+        <section className="book-details">
             <h1>Book Details</h1>
-        </div>
+
+            <div className="book-details-container flex">
+
+                <button onClick={onLeftArrow}>◀</button>
+                <article className="book-show-details">
+                    <div className="details-title">
+                        <h2>Title {books[index].title}</h2>
+                        <p>checkbox: {books[index].isInWishlist}</p>
+                    </div>
+                    <h3 className="details-author">Author: {books[index].author}</h3>
+                    <h6 className="details-description">Description: {books[index].description}</h6>
+                    <div className="details-rating">
+                        <h8>Rating:</h8>
+                        <h8>{books[index].rating}</h8>
+                    </div>
+                    <div className="details-price">
+                        <span>Price:</span>
+                        <span>${books[index].price}</span>
+                    </div>
+                </article>
+                <button onClick={onRightArrow}>▶</button>
+
+
+            </div>
+
+        </section>
     )
 }
