662b0e52ce134c367c3e9d2de8b0bc4b8d1b5c7a
Karam Isa <karam_isa@icloud.com>
2023-02-05 13:59:18 +0200

    loadbooks for bookapp

--- book-wishlist/src/views/book-app.jsx
+++ book-wishlist/src/views/book-app.jsx
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
 import { BookDetails } from "../cmps/book-details";
 import { BookWishlist } from "../cmps/book-wishlist";
+import { bookService } from "../services/book.service";
 
 export function BookApp(){
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+       loadBooks()
+    }, [])
+
+    async function loadBooks(){
+        const books = await bookService.query()
+        setBooks(books)
+    }
 
     return (
         <div className="flex">
-        <BookDetails />
-        <BookWishlist />
+        <BookDetails books={books}/>
+        <BookWishlist books={books} />
         </div>
     )
 }
